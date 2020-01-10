b1e9ffda57062639f704fdce7280b516398a8c94
HP PC <HP PC@DESKTOP-9077T2E>
2020-01-10 15:34:05 +0530

    commiting1st

---
+++ MAVENFIRST/src/test/java/Mav123/MAVENFIRST/Demo.java
@@ -0,0 +1,11 @@
+package Mav123.MAVENFIRST;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Maven rinning1 in Demo");
+
+	}
+
+}
---
+++ MAVENFIRST/src/test/java/Mav123/MAVENFIRST/Test.java
@@ -0,0 +1,10 @@
+package Mav123.MAVENFIRST;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Maven rinning2 in Test");
+
+	}
+
+}
